c016f721053e2ad86d07c65195f442ddbb773189
lorreina <slorreina369@gmail.com>
2022-08-04 22:28:22 -0700

    users function created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes');
 
-module.exports = router
+router.use('/users', userRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const session = require('express-session');
 const path = require('path');
 const exphns = require('express-handlebars');
-const sequelize = require('sequelize');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 //app.use(session(sess));
-app.set('view engine', 'handlebars');
+//app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
