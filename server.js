726429e84f9def739a6cdc53881c08a9e4fbbc94
lorreina <slorreina369@gmail.com>
2022-08-05 22:00:11 -0700

    uploaded to heroku and changes made to controllers

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const session = require('express-session');
+
 const path = require('path');
-const exphns = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 //app.use(session(sess));
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
